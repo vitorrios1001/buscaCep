4b1be713b766b2c90f5bbad3b57e9e961649ffd6
Vitor Rios <vitorluizrios@hotmail.com>
2018-05-08 23:54:26 -0300

    adding antd

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,47 +1,57 @@
 import React, { Component } from 'react';
-
+import 'antd/dist/antd.css';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Button } from 'antd';
+import { Button, Card } from 'antd';
 
 import minhaFuncao from '../actions';
 
 class App extends Component {
 
   state = {
-    cep: ''
+    cep: '',
+    searching: true
+  }
+
+  findCep = () => {
+    this.props.minhaFuncao(this.state.cep);
   }
 
   enter = (e) => {
     console.log(e);
-    if(e.charCode === 13)
-     this.props.minhaFuncao(this.state.cep);
+    if (e.charCode === 13)
+      this.findCep();
   }
 
   render() {
     return (
-      <div className="div">
+      <div className="div" id="container" >
+        <Card id="cardMain" title="Consulta de CEP" style={{ width: 500 }}>
+          <h3>
+            Digite seu Cep:
+            <input
+              value={this.state.cep}
+              onChange={event => this.setState({ cep: event.target.value })}
+              onKeyPress={event => this.enter(event)}
+            />
 
-        <h2>
-          Digite seu Cep:
-          <input
-            value={this.state.cep}
-            onChange={event => this.setState({ cep: event.target.value })}
-            onKeyPress={ event => this.enter(event) }
-          />                    
-          <input type="submit" value="Buscar CEP" onClick={ () => this.props.minhaFuncao(this.state.cep) } /> 
-        </h2>        
-        <small><br />
-          {this.props.teste.meuCep.logradouro}
-        </small>
-        <small><br />
-          {this.props.teste.meuCep.bairro}
-        </small>
-        <small><br />
-          {this.props.teste.meuCep.localidade}-{this.props.teste.meuCep.uf}
-        </small>
+            <Button
+              type="primary"
+              shape="circle"
+              icon="search"
+              onClick={() => this.findCep()}
+            />
+
+          </h3>
+          <div style={{ alignContent: "left" }}>
+            <p>Logradouro: {this.props.teste.meuCep.logradouro}</p>
+            <p>Complemento: {this.props.teste.meuCep.complemento}</p>
+            <p>Bairro: {this.props.teste.meuCep.bairro}</p>
+            <p>Cidade: {this.props.teste.meuCep.localidade}</p>
+            <p>Estado: {this.props.teste.meuCep.uf}</p>
+          </div>
+        </Card>
       </div>
-
     )
   }
 }
